b3f86b639ad646662cac97f38ffd772e971fd11a
Nicolas Hz <nicolasholzman@hotmail.com>
2018-02-21 17:32:27 -0300

    added full calendar library

--- src/app/modules/birthday/birthday.component.ts
+++ src/app/modules/birthday/birthday.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import 'fullcalendar';
+import 'fullcalendar-scheduler';
+declare let $: any;
 @Component({
   selector: 'app-birthday',
   templateUrl: './birthday.component.html',
@@ -10,6 +12,7 @@ export class BirthdayComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('#calendar').fullCalendar({});
   }
 
 }
